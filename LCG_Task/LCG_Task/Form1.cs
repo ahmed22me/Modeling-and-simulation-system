4ed5f37e57c0b6c9a7420bbece43da0b9ed27fd4
ahmed22me <as7211018@gmail.com>
2024-07-13 17:26:13 +0300

    first commit

---
+++ LCG_Task/LCG_Task/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LCG_Task
+{
+    public partial class Form1 : Form
+    { 
+       // int[] itration;
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void clearData()
+        {
+            this.textBox1.Text = "";
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
+            this.textBox4.Text = "";
+            this.textBox5.Text = "";
+            this.textBox6.Text = "";
+            this.dataGridView1.Rows.Clear();
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            long Xo = long.Parse(textBox4.Text); // Seed value
+            long m = long.Parse(textBox2.Text); // Modulus parameter
+            long a = long.Parse(textBox5.Text); // Multiplier term
+            long c = long.Parse(textBox3.Text); // Increment term
+          long iterationsofrandam = (long.Parse(textBox1.Text)+1); // Number of Random numbers
+            long[] randomNums = new long[iterationsofrandam];
+            LCG_Task.lcg.linearCongruentialMethod(Xo, m, a, c, randomNums, iterationsofrandam);
+
+           long con = 0;
+            for (long i = 0; i < iterationsofrandam; i++)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[1].Value = randomNums[i];
+                row.Cells[0].Value = con++;
+                dataGridView1.Rows.Add(row);
+          
+            }
+           long coun=1;
+            
+            textBox6.Text = LCG_Task.lcg.cyclelenth(Xo, c, m,a, coun, randomNums, iterationsofrandam).ToString();
+
+
+
+
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearData();
+        }
+    }
+}
---
+++ LCG_Task/LCG_Task/lcg.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace LCG_Task
+{
+    internal class lcg
+    {
+        public static void linearCongruentialMethod(long Xo, long m, long a, long c, long[] rand_no, long n_iterations)
+        {
+            rand_no[0] = Xo;
+
+            for (long i = 1; i < n_iterations; i++)
+            {
+                rand_no[i] = (rand_no[i - 1] * a + c) % m;
+            }
+        }
+
+        public static long cycleLength(long Xo, long c, long m, long a, long longest_period, long[] randomNums, long n_iterations)
+        {
+            long k = m - 1;
+
+            if (IsRelativePrime(c, m) && a == 4 * k + 1 && power_two(m) && c > 0)
+            {
+                longest_period = m;
+            }
+            else if (checkPrime(m) && checkDivisibleM(a, m) && c == 0)
+            {
+                longest_period = m - 1;
+            }
+            else if (checkOdd(Xo) && power_two(m) && (a == 5 + 8 * k || a == 3 + 8 * k) && c == 0)
+            {
+                longest_period = m / 4;
+            }
+            else
+            {
+                longest_period = 1;
+            }
+
+            return longest_period;
+        }
+
+        static bool power_two(long M)
+        {
+            if (M == 0)
+                return false;
+
+            while (M != 1)
+            {
+                if (M % 2 != 0)
+                    return false;
+                M = M / 2;
+            }
+            return true;
+        }
+
+       
+
+        public static bool IsRelativePrime(long firstNum, long secondNum)
+        {
+            if (firstNum == secondNum || firstNum % secondNum == 0 || secondNum % firstNum == 0)
+            {
+                return false;
+            }
+            else if (firstNum > secondNum)
+            {
+                for (long i = 2; i <= secondNum / 2; i++)
+                {
+                    if (firstNum % i == 0 && secondNum % i == 0)
+                        return false;
+                }
+                return true;
+            }
+            else
+            {
+                for (long i = 2; i <= firstNum / 2; i++)
+                {
+                    if (firstNum % i == 0 && secondNum % i == 0)
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        // Check if m is a prime number
+        public static bool checkPrime(long m)
+        {
+            if (m < 2)
+            {
+                return false;
+            }
+
+            for (long i = 2; i <= Math.Sqrt(m); i++)
+            {
+                if (m % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool checkDivisibleM(long a, long m)
+        {
+            long k = m - 1;
+            double div = Math.Pow(a, k) - 1;
+
+            if (div % m == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool checkOdd(long Xo)
+        {
+            return Xo % 2 != 0;
+        }
+    }
+}
